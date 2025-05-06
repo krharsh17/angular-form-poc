9ee4d34ac5a20f411dc390ec226bd6a94d3ac23a
Kumar Harsh <hey@kumarharsh.me>
2025-05-06 21:04:04 +0530

    Add HTML form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ContactFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
